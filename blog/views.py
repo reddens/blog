48c248f0e9b62847228f2c804bbe38411064c2c4
CYril <cyrilokworu@protonmail.com>
2021-07-20 00:16:14 +0100

    added templates

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse 
+from django.shortcuts import render 
 
 def about(request): 
-    return HttpResponse("about")
-
+    #return HttpResponse("about")
+    return render(request, "about.html")
 def home(request): 
-    return HttpResponse("home")
+    #return HttpResponse("home")
+    return render(request, "homepage.html")
