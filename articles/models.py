03b0e9de181a12b5b0c36e383c72709222ee576c
CYril <cyrilokworu@protonmail.com>
2021-07-28 17:39:58 +0100

    added styles and post creation functionality

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models.fields import TextField
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -8,7 +8,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default="default.jpg", blank=True)
-    # add in author later 
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
